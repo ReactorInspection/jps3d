c6b3e90278a18e227297fe9e8233aaf1d0e0c13d
laura <laurajar@seas.upenn.edu>
2018-09-13 12:33:08 -0400

    kind of nasty tf handling in order to give goal/start in map frame

--- test/plan3d_to_path.cpp
+++ test/plan3d_to_path.cpp
@@ -5,6 +5,9 @@
 #include <nav_msgs/Path.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <ros/ros.h>
+#include <tf/transform_listener.h>
+#include <tf/tf.h>
+
 
 
 ros::Publisher path_pub;
@@ -20,15 +23,46 @@ nav_msgs::Path plan(char* map){
 
 
   nav_msgs::Path path;
-  path.header.frame_id = "map";
+  path.header.frame_id = "yaml";
   path.header.stamp = ros::Time();
   
   // store map in map_util
   std::shared_ptr<VoxelMapUtil> map_util = std::make_shared<VoxelMapUtil>();
   map_util->setMap(reader.origin(), reader.dim(), reader.data(), reader.resolution());
 
-  const Vec3f start(reader.start(0), reader.start(1), reader.start(2));
-  const Vec3f goal(reader.goal(0), reader.goal(1), reader.goal(2));
+  // give goals in the map frame, but give to planner in the yaml frame
+  const Vec3f start_map(0.2,0.2,0.2);
+  //~ const Vec3f goal(reader.goal(0), reader.goal(1), reader.goal(2));
+  const Vec3f goal_map(11.5,4,17);
+  // 2 4 12 9.5 0 5
+  tf::StampedTransform transform;
+  tf::TransformListener listener;
+  geometry_msgs::PoseStamped ps_start;
+  geometry_msgs::PoseStamped ps_goal;
+  ps_start.pose.position.x = start_map[0];
+  ps_start.pose.position.y = start_map[1];
+  ps_start.pose.position.z = start_map[2];
+  ps_start.pose.orientation.w = 1;
+  ps_goal.pose.position.x = goal_map[0];
+  ps_goal.pose.position.y = goal_map[1];
+  ps_goal.pose.position.z = goal_map[2];
+  ps_goal.pose.orientation.w = 1;
+  ps_start.header.frame_id = "map";
+  ps_goal.header.frame_id = "map";
+
+  try{
+	listener.waitForTransform("yaml", "map", ros::Time(0), ros::Duration(2));
+	listener.lookupTransform("yaml", "map", ros::Time(0), transform);
+	listener.transformPose("yaml",ps_start, ps_start);
+	listener.transformPose("yaml",ps_goal, ps_goal);
+   }
+   catch (tf::TransformException ex){
+      ROS_ERROR("%s",ex.what());
+      ros::Duration(1.0).sleep();
+   }
+	const Vec3f start(ps_start.pose.position.x,ps_start.pose.position.y,ps_start.pose.position.z);
+	const Vec3f goal(ps_goal.pose.position.x,ps_goal.pose.position.y,ps_goal.pose.position.z);
+
 
   std::unique_ptr<JPSPlanner3D> planner_ptr(new JPSPlanner3D(true)); // Declare a planner
   planner_ptr->setMapUtil(map_util); // Set collision checking function
@@ -39,7 +73,7 @@ nav_msgs::Path plan(char* map){
   auto path_jps = planner_ptr->getRawPath();
   for(const auto& it: path_jps){
 	geometry_msgs::PoseStamped posestamp;
-	posestamp.header.frame_id = "map";
+	posestamp.header.frame_id = "yaml";
 	posestamp.header.stamp = ros::Time();
 	posestamp.pose.position.x = it.transpose()[0];
 	posestamp.pose.position.y = it.transpose()[1];
