4fa3f74f0c3d43f35320ad15af7e2d5a3925562c
laura <laurajar@seas.upenn.edu>
2018-09-13 13:05:51 -0400

    working with waypoint publisher

--- test/plan3d_to_path.cpp
+++ test/plan3d_to_path.cpp
@@ -11,44 +11,65 @@
 
 
 ros::Publisher path_pub;
+geometry_msgs::PoseStamped ps_start;
+geometry_msgs::PoseStamped ps_goal;
+bool replan_flag;
+
 using namespace JPS;
 
-nav_msgs::Path plan(char* map){
-  
+void waypointsCallback(const nav_msgs::Path::ConstPtr& msg){
+	ROS_INFO("waypoints received. start and/or goal moved");
+	if (msg->poses.size()>2) ROS_WARN("first 2 waypoints treated as start, goal. have not yet implemented handling additional points");
+	ps_start = msg->poses[0];
+	ps_goal = msg->poses[1];
+	ps_start.header.frame_id = msg->header.frame_id;
+	ps_goal.header.frame_id = msg->header.frame_id;
+	replan_flag = true;
+}
+
+std::shared_ptr<VoxelMapUtil> readMap(char* map){
   // Read the map from yaml
+  ROS_INFO("reading map (takes a few seconds)");
   MapReader<Vec3i, Vec3f> reader(map, true); // Map read from a given file
   if(!reader.exist()) {
     printf(ANSI_COLOR_RED "Cannot read input file [%s]!\n" ANSI_COLOR_RESET, map);
   }
-
-
-  nav_msgs::Path path;
-  path.header.frame_id = "yaml";
-  path.header.stamp = ros::Time();
   
   // store map in map_util
   std::shared_ptr<VoxelMapUtil> map_util = std::make_shared<VoxelMapUtil>();
   map_util->setMap(reader.origin(), reader.dim(), reader.data(), reader.resolution());
+  return map_util;
+}
 
-  // give goals in the map frame, but give to planner in the yaml frame
-  const Vec3f start_map(0.2,0.2,0.2);
-  //~ const Vec3f goal(reader.goal(0), reader.goal(1), reader.goal(2));
-  const Vec3f goal_map(11.5,4,17);
-  // 2 4 12 9.5 0 5
+nav_msgs::Path plan(std::shared_ptr<VoxelMapUtil>  map_util){
+  nav_msgs::Path path;
+  path.header.frame_id = "yaml";
+  path.header.stamp = ros::Time();
+  
+  if (ps_goal.pose.position.x == ps_start.pose.position.x && ps_goal.pose.position.y == ps_start.pose.position.y && ps_goal.pose.position.z == ps_start.pose.position.z){
+	   ROS_ERROR("start and goal poses are the same. Have you published waypoints?");
+	   return path;
+   }
+   else ROS_INFO("running planner");
+
+  //~ // give goals in the map frame, but give to planner in the yaml frame
+  //~ const Vec3f start_map(0.2,0.2,0.2);
+  //~ const Vec3f goal_map(11.5,4,17);
+  //~ // 2 4 12 9.5 0 5
+  //~ geometry_msgs::PoseStamped ps_start;
+  //~ geometry_msgs::PoseStamped ps_goal;
+  //~ ps_start.pose.position.x = start_map[0];
+  //~ ps_start.pose.position.y = start_map[1];
+  //~ ps_start.pose.position.z = start_map[2];
+  //~ ps_start.pose.orientation.w = 1;
+  //~ ps_goal.pose.position.x = goal_map[0];
+  //~ ps_goal.pose.position.y = goal_map[1];
+  //~ ps_goal.pose.position.z = goal_map[2];
+  //~ ps_goal.pose.orientation.w = 1;
+  //~ ps_start.header.frame_id = "map";
+  //~ ps_goal.header.frame_id = "map";
   tf::StampedTransform transform;
   tf::TransformListener listener;
-  geometry_msgs::PoseStamped ps_start;
-  geometry_msgs::PoseStamped ps_goal;
-  ps_start.pose.position.x = start_map[0];
-  ps_start.pose.position.y = start_map[1];
-  ps_start.pose.position.z = start_map[2];
-  ps_start.pose.orientation.w = 1;
-  ps_goal.pose.position.x = goal_map[0];
-  ps_goal.pose.position.y = goal_map[1];
-  ps_goal.pose.position.z = goal_map[2];
-  ps_goal.pose.orientation.w = 1;
-  ps_start.header.frame_id = "map";
-  ps_goal.header.frame_id = "map";
 
   try{
 	listener.waitForTransform("yaml", "map", ros::Time(0), ros::Duration(2));
@@ -88,15 +109,23 @@ int main(int argc, char** argv) {
     ros::init(argc, argv, "plan3d_to_path");
     ros::NodeHandle nh("~");
     path_pub = nh.advertise<nav_msgs::Path>("path", 1);
+	ros::Subscriber sub = nh.subscribe("/waypoints", 1000, waypointsCallback);
+
 	if(argc != 2) {
 		printf(ANSI_COLOR_RED "Input yaml required!\n" ANSI_COLOR_RESET);
 		return -1;
 	}
-	nav_msgs::Path path = plan(argv[1]);
+	std::shared_ptr<VoxelMapUtil> map_util = readMap(argv[1]);
+	nav_msgs::Path path;
+	replan_flag = true;
 	while (ros::ok()){
+		  ros::spinOnce();
+		  if (replan_flag){
+			path = plan(map_util);
+			replan_flag = false;
+		  }
 		  path_pub.publish(path);
-		  ros::Duration(5).sleep();
+		  ros::Duration(1).sleep();
 	}
-		//~ ros::spinOnce();
     return 0;
 }
