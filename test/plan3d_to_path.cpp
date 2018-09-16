9abbb41b9bc166ae7e1b20fe43d6fa4c7475d9ef
laura <laurajar@seas.upenn.edu>
2018-09-16 01:40:19 -0400

    dmplanner working, but has strange behavior on replan

--- test/plan3d_to_path.cpp
+++ test/plan3d_to_path.cpp
@@ -2,6 +2,8 @@
 #include "read_map.hpp"
 #include <jps_basis/data_utils.h>
 #include <jps_planner/jps_planner/jps_planner.h>
+#include <jps_planner/distance_map_planner/distance_map_planner.h>
+
 #include <nav_msgs/Path.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <ros/ros.h>
@@ -11,6 +13,7 @@
 
 
 ros::Publisher path_pub;
+ros::Publisher dmp_path_pub;
 geometry_msgs::PoseStamped ps_start;
 geometry_msgs::PoseStamped ps_goal;
 bool replan_flag;
@@ -45,7 +48,25 @@ std::shared_ptr<VoxelMapUtil> readMap(char* map){
   return map_util;
 }
 
-nav_msgs::Path plan(std::shared_ptr<VoxelMapUtil>  map_util){
+std::shared_ptr<JPSPlanner3D> setUpJPS(std::shared_ptr<VoxelMapUtil>  map_util){
+  std::shared_ptr<JPSPlanner3D> planner_ptr(new JPSPlanner3D(true)); // Declare a planner
+  planner_ptr->setMapUtil(map_util); // Set collision checking function
+  planner_ptr->updateMap();
+  return planner_ptr;
+}
+
+std::shared_ptr<DMPlanner3D> setUpDMP(std::shared_ptr<VoxelMapUtil>  map_util){
+  std::shared_ptr<DMPlanner3D> dmplanner_ptr(new DMPlanner3D(true)); // Declare a planner
+  dmplanner_ptr->setMapUtil(map_util); // Set collision checking function
+  dmplanner_ptr->updateMap();
+  dmplanner_ptr->setPotentialRadius(Vec3f(1.0, 1.0, 1.0)); // Set 2D potential field radius
+  dmplanner_ptr->setSearchRadius(Vec3f(0.5, 0.5, 0.5)); // Set the valid search region around given path
+  return dmplanner_ptr;
+}
+
+
+
+nav_msgs::Path do_planning(std::shared_ptr<JPSPlanner3D> planner_ptr , std::shared_ptr<DMPlanner3D> dmplanner_ptr){
   nav_msgs::Path path;
   path.header.frame_id = "yaml";
   path.header.stamp = ros::Time();
@@ -57,21 +78,6 @@ nav_msgs::Path plan(std::shared_ptr<VoxelMapUtil>  map_util){
    else ROS_INFO("running planner");
 
   //~ // give goals in the map frame, but give to planner in the yaml frame
-  //~ const Vec3f start_map(0.2,0.2,0.2);
-  //~ const Vec3f goal_map(11.5,4,17);
-  //~ // 2 4 12 9.5 0 5
-  //~ geometry_msgs::PoseStamped ps_start;
-  //~ geometry_msgs::PoseStamped ps_goal;
-  //~ ps_start.pose.position.x = start_map[0];
-  //~ ps_start.pose.position.y = start_map[1];
-  //~ ps_start.pose.position.z = start_map[2];
-  //~ ps_start.pose.orientation.w = 1;
-  //~ ps_goal.pose.position.x = goal_map[0];
-  //~ ps_goal.pose.position.y = goal_map[1];
-  //~ ps_goal.pose.position.z = goal_map[2];
-  //~ ps_goal.pose.orientation.w = 1;
-  //~ ps_start.header.frame_id = "map";
-  //~ ps_goal.header.frame_id = "map";
   tf::StampedTransform transform;
   tf::TransformListener listener;
 
@@ -88,21 +94,33 @@ nav_msgs::Path plan(std::shared_ptr<VoxelMapUtil>  map_util){
 	const Vec3f start(ps_start.pose.position.x,ps_start.pose.position.y,ps_start.pose.position.z);
 	const Vec3f goal(ps_goal.pose.position.x,ps_goal.pose.position.y,ps_goal.pose.position.z);
 
-  std::unique_ptr<JPSPlanner3D> planner_ptr(new JPSPlanner3D(true)); // Declare a planner
-  planner_ptr->setMapUtil(map_util); // Set collision checking function
-  planner_ptr->updateMap();
 
   planner_ptr->plan(start, goal, 1, true); // Plan from start to goal using JPS
 
   auto path_jps = planner_ptr->getRawPath();
+  geometry_msgs::PoseStamped posestamp;
+  posestamp.header.frame_id = "yaml";
   for(const auto& it: path_jps){
-	geometry_msgs::PoseStamped posestamp;
+	posestamp.header.stamp = ros::Time();
+	posestamp.pose.position.x = it.transpose()[0];
+	posestamp.pose.position.y = it.transpose()[1];
+	posestamp.pose.position.z = it.transpose()[2];
+	path.poses.push_back(posestamp);
+  }
+  
+  // Run DMP planner
+  dmplanner_ptr->computePath(start, goal, path_jps); // Compute the path given the jps path
+  const auto path_dmp = dmplanner_ptr->getPath();
+  path.poses.clear();
+  for(const auto& it: path_dmp){
 	posestamp.header.frame_id = "yaml";
 	posestamp.header.stamp = ros::Time();
 	posestamp.pose.position.x = it.transpose()[0];
 	posestamp.pose.position.y = it.transpose()[1];
 	posestamp.pose.position.z = it.transpose()[2];
 	path.poses.push_back(posestamp);
+    dmp_path_pub.publish(path);
+
   }
   return path;
   
@@ -112,19 +130,23 @@ int main(int argc, char** argv) {
     ros::init(argc, argv, "plan3d_to_path");
     ros::NodeHandle nh("~");
     path_pub = nh.advertise<nav_msgs::Path>("path", 1);
+    path_pub = nh.advertise<nav_msgs::Path>("dmp_path", 1);
 	ros::Subscriber sub = nh.subscribe("/waypoints", 1000, waypointsCallback);
 
 	if(argc != 2) {
 		printf(ANSI_COLOR_RED "Input yaml required!\n" ANSI_COLOR_RESET);
 		return -1;
 	}
+	
 	std::shared_ptr<VoxelMapUtil> map_util = readMap(argv[1]);
+	std::shared_ptr<JPSPlanner3D> planner_ptr = setUpJPS(map_util);
+	std::shared_ptr<DMPlanner3D> dmplanner_ptr = setUpDMP(map_util);
 	nav_msgs::Path path;
 	replan_flag = true;
 	while (ros::ok()){
 		  ros::spinOnce();
 		  if (replan_flag){
-			path = plan(map_util);
+			path = do_planning(planner_ptr, dmplanner_ptr);
 			replan_flag = false;
 		  }
 		  path_pub.publish(path);
